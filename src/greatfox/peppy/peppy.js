2c6a06861bc960c740c654345e0063521a06313f
austen-wade <austen.wade.ut@gmail.com>
2018-11-07 11:05:32 -0700

    added tricky

--- src/greatfox/peppy/peppy.js
+++ src/greatfox/peppy/peppy.js
@@ -1,5 +1,9 @@
 /* Dropdown */
 
+/*
+  Goal: dropdown should not go offscreen and should attach to trigger point
+*/
+
 var Peppy = function() {
   "use strict";
   var init = function(opt) {
---
+++ src/greatfox/tricky/tricky.js
@@ -0,0 +1,5 @@
+/* Select */
+
+/*
+  Goal: Custom select dropdown with options. Disabled and default attributes should have an effect.
+*/
