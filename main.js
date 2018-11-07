8d1c8f13301c112278948d991ed77dcc88cbcff6
austen-wade <austen.wade.ut@gmail.com>
2018-11-07 10:33:46 -0700

    basic slippy code

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 import "./src/falco/falco.scss";
 import Falco from "./src/falco/falco";
+import "./src/fox/fox.scss";
 import Fox from "./src/fox/fox";
+import "./src/slippy/slippy.scss";
 import Slippy from "./src/slippy/slippy";
+import "./src/peppy/peppy.scss";
 import Peppy from "./src/peppy/peppy";
 
 var modalInfo = new Falco();
@@ -12,11 +15,11 @@ var dropdown = new Peppy();
 accordion.init();
 dropdown.init();
 
-window.addEventListener('scroll', function() {
+window.addEventListener("scroll", function() {
   scrollNav.init({
-    className: '.scroll-nav'
+    className: ".scroll-nav"
   });
-})
+});
 
 modalInfo.init({
   name: "info",
--- src/slippy/slippy.js
+++ src/slippy/slippy.js
@@ -1,13 +1,53 @@
 /* Accordion */
 
 var Slippy = function() {
+  var accordion,
+      accordionHeader,
+      accordionContent;
+
   var init = function(opt) {
-    console.log("Slippy init");
+    accordion = document.querySelector('[accordion]');
+    accordionHeader = accordion.querySelector('[accordion-title]');
+    accordionContent = accordion.querySelector('[accordion-content]');
+
+    accordion.addEventListener('click', function() {
+      if (accordion.classList.contains('open')) {
+        close();
+      } else {
+        open();
+      }
+    });
+
+    close();
+
+    console.log(accordion, accordionHeader, accordionContent);
   };
 
+  var open = function() {
+    accordion.classList.add('open');
+    accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px';
+  }
+
+  var close = function() {
+    accordion.classList.remove('open');
+    accordionContent.style.maxHeight = 0;
+  }
+
   return {
-    init: init
+    init: init,
+    open: open,
+    close: close
   };
 };
 
 module.exports = Slippy;
+
+
+// const acc = document.querySelectorAll("button.accordion");
+// for (let o of acc) o.addEventListener("click", (e) => {
+//   e.target.classList.toggle("active");
+//   let panel = e.target.nextElementSibling;
+//   panel.style.maxHeight
+//     ? (panel.style.maxHeight = null)
+//     : (panel.style.maxHeight = panel.scrollHeight + "px");
+// });
